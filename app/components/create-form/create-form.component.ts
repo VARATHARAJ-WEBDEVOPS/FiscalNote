e21df4b9f4b673106397d43c2bd745722ca09fdc
S.Varatharaj <133386133+VARATHARAJ-WEBDEVOPS@users.noreply.github.com>
2023-07-11 12:21:08 +0530

    Update create-form.component.ts

--- app/components/create-form/create-form.component.ts
+++ app/components/create-form/create-form.component.ts
@@ -1,9 +1,9 @@
-import { Component, ViewChild, ViewContainerRef,OnInit, ElementRef} from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, OnInit, ElementRef } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ServiceService } from 'src/app/service/service.service';
 import Swal from 'sweetalert2';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-form',
@@ -11,36 +11,31 @@ import {  Router } from '@angular/router';
   styleUrls: ['./create-form.component.css']
 })
 export class CreateFormComponent implements OnInit {
-  
   employeeFormGroup!: FormGroup;
-
   Email: any;
-  balref  : any ;
-  displayedColumns: string[] = ['Persolnal Detail', 'Date','Due Date', 'Amount','Interest', 'Actions'];  
+  balref: any;
+  displayedColumns: string[] = ['Persolnal Detail', 'Date', 'Due Date', 'Amount', 'Interest', 'Actions'];
   reloadFlag = localStorage.getItem('reloadFlag');
-
   containerRef!: ViewContainerRef;
   @ViewChild(MatSidenav)
-
   sidenav!: MatSidenav;
   formGroup: any;
-  
-  constructor( private fb: FormBuilder,
+
+  constructor(
+    private fb: FormBuilder,
     private elementRef: ElementRef,
-    public service:ServiceService,
+    public service: ServiceService,
     private router: Router
-    )
-  { }  
+  ) {}
 
-  ngOnInit(): void {  
-
-    this.employeeFormGroup = this.fb.group({   //Getting Form Inputs      
+  ngOnInit(): void {
+    this.employeeFormGroup = this.fb.group({
       '_id': [],
       '_rev': [],
-      'employee_name': ['',[Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
-      'phone':['',[Validators.required, Validators.pattern(/^[0-9]{10}$/)]],      
-      'date': ['',Validators.required],
-      'address': ['',Validators.required],
+      'employee_name': ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
+      'phone': ['', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]],
+      'date': ['', Validators.required],
+      'address': ['', Validators.required],
       'duedate': ['', [Validators.required, Validators.pattern(/^\d+$/)]],
       'interest': ['', [Validators.required, Validators.pattern(/^\d+$/)]],
       amount: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
@@ -48,7 +43,7 @@ export class CreateFormComponent implements OnInit {
       profit: [],
       finalamt: [],
       balanceMonth: [],
-      amtPerMonth: [],
+      amtPerMonth: []
     });
 
     if (!this.reloadFlag) {
@@ -56,75 +51,73 @@ export class CreateFormComponent implements OnInit {
       location.reload();
     }
 
-    if( localStorage.getItem('email') == null){
-      Swal.fire('UnAutharized User', '', 'warning')
+    if (localStorage.getItem('email') == null) {
+      Swal.fire('UnAuthorized User', '', 'warning');
       this.router.navigateByUrl('/');
     }
-    this.Email = localStorage.getItem('email');     
-   
-  }  
-  printx(){
-    localStorage.setItem('reloadFlag', "")
+    this.Email = localStorage.getItem('email');
+  }
+
+  printx() {
+    localStorage.setItem('reloadFlag', '');
   }
 
   calculate() {
-    const amt : number= this.employeeFormGroup.value.amount;
-    const intr : number  = this.employeeFormGroup.value.interest;
-    const due : number = this.employeeFormGroup.value.duedate;
-    const prof  : number =  amt * intr * due /100 ;
-    const bal : number = prof + amt; 
-    const amtpermon : Number = bal / due ;
+    const amt: number = this.employeeFormGroup.value.amount;
+    const intr: number = this.employeeFormGroup.value.interest;
+    const due: number = this.employeeFormGroup.value.duedate;
+    const prof: number = amt * intr * due / 100;
+    const bal: number = prof + amt;
+    const amtpermon: Number = bal / due;
     this.employeeFormGroup.value.profit = prof;
-    this.employeeFormGroup.value.balance = bal ;
-    this.employeeFormGroup.value.finalamt = bal ;
+    this.employeeFormGroup.value.balance = bal;
+    this.employeeFormGroup.value.finalamt = bal;
     this.employeeFormGroup.value.amtPerMonth = amtpermon;
     this.employeeFormGroup.value.balanceMonth = due;
   }
 
   editbalance() {
-    const balinput  = this.employeeFormGroup.value.balance;
+    const balinput = this.employeeFormGroup.value.balance;
     const amtPerMonth = this.employeeFormGroup.value.amtPerMonth;
     const baldue = this.employeeFormGroup.value.balanceMonth;
     const balfix: Number = balinput - amtPerMonth;
     this.employeeFormGroup.value.balanceMonth = baldue - 1;
-    this.employeeFormGroup.value.balance = balfix ;
+    this.employeeFormGroup.value.balance = balfix;
   }
-  
-  saveAction() : void{                            
-   
-    if (!this.employeeFormGroup.valid ) {
-      Swal.fire('Please Check All the Fields are Filled!', '', 'warning')
-    }else{
-      let employeeObject:any = this.employeeFormGroup.value;
-      console.log(employeeObject);   
-      employeeObject['object_name'] = 'staff_name' 
+
+  saveAction(): void {
+    if (!this.employeeFormGroup.valid) {
+      Swal.fire('Please Check All the Fields are Filled!', '', 'warning');
+    } else {
+      let employeeObject: any = this.employeeFormGroup.value;
+      console.log(employeeObject);
+      employeeObject['object_name'] = 'staff_name';
 
       if (employeeObject['_id'] == null) {
-        delete employeeObject['_id']
+        delete employeeObject['_id'];
       }
 
       if (employeeObject['_rev'] == null) {
-        delete employeeObject['_rev']
-      }     
-   
-      let bulkDocsArray = []
-      bulkDocsArray.push(employeeObject)
-      this.service.updateDocument(bulkDocsArray)
+        delete employeeObject['_rev'];
+      }
+
+      let bulkDocsArray = [];
+      bulkDocsArray.push(employeeObject);
+      this.service.updateDocument(bulkDocsArray);
 
       this.resetAction();
-      Swal.fire('Saved Successfully' ,'' ,'success');
-    }     
+      Swal.fire('Saved Successfully', '', 'success');
+    }
   }
 
   resetAction(): void {
-    this.employeeFormGroup.reset()
-    this.employeeFormGroup.markAsUntouched()
-  }  
+    this.employeeFormGroup.reset();
+    this.employeeFormGroup.markAsUntouched();
+  }
 
   logout() {
     localStorage.removeItem('email');
     this.router.navigateByUrl('/');
-    localStorage.setItem('reloadFlag', "")
+    localStorage.setItem('reloadFlag', '');
   }
- 
 }
